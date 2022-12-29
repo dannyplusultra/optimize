e449434b5401f75cac0acc7e7f6bd9b1068f195f
Danny Del Cerro <danny,del_cerro@keysight.com>
2022-12-29 10:54:50 -0800

    added file chooser

--- src/PGA_Optimizer.java
+++ src/PGA_Optimizer.java
@@ -7,8 +7,9 @@ public class PGA_Optimizer {
     private JPanel PGA_Optimizer_Panel;
     private JPanel Start;
     private JButton Exit_Button;
+    private JButton fielButton;
 
-public PGA_Optimizer() {
+    public PGA_Optimizer() {
     JFrame frame = new JFrame("PGA Optimizer");
     frame.setContentPane(PGA_Optimizer_Panel);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -24,7 +25,16 @@ public PGA_Optimizer() {
             }
         }
     });
-}
+        fielButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource()==fielButton) {
+                   JFileChooser fileChooser = new JFileChooser();
+                   fileChooser.showOpenDialog(null);
+                }
+            }
+        });
+    }
 
     public JPanel getPGA_Optimizer_Panel() {
         return PGA_Optimizer_Panel;
