6e9db77a5a752dd942d85da44e70ed3cfa004545
Danny Del Cerro <danny,del_cerro@keysight.com>
2022-12-29 15:08:45 -0800

    added mkp

--- src/PGA_Optimizer.java
+++ src/PGA_Optimizer.java
@@ -79,11 +79,38 @@ public class PGA_Optimizer {
         RunButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                for (int x = 0; x < numlineups; x++) {
+                PGA_Players[] items = (PGA_Players[]) Players.toArray();
+                System.out.print("We have the following " + items.length + " items (value, size, volume): ");
+                for(int i = 0; i < items.length; i++)
+                    System.out.print("(" + items[i].Projection + ", " + items[i].Salary + ", " + items[i].Ownership + ") ");
+                System.out.println();
+                System.out.println("And a knapsack of size " + salary + " and volume " + maxprod);
+
+                noRep(items);
+                System.out.println();
+                // Print the solution
+                double j = salary, h = maxprod, finalSize = 0, finalValue = 0, finalVolume = 0;
+                System.out.print("Items picked (value, size, volume) for 0/1 problems without repetitions: ");
+                for(int i = items.length; i > 0; i--) {
+                    if(dynNoRep[i][(int)j][(int)h] != dynNoRep[i - 1][(int)j][(int)h]) {
+                        System.out.print("(" + items[i - 1].Projection + ", " + items[i - 1].Salary + ", " + items[i - 1].Ownership + ") ");
+                        finalSize += items[i - 1].Salary;
+                        finalValue += items[i - 1].Projection;
+                        finalVolume += items[i - 1].Ownership;
+                        j -= items[i - 1].Salary;
+                        h -= items[i - 1].Ownership;
+                    }
+                }
+                System.out.println();
+                System.out.println(" Final size: " + finalSize);
+                System.out.println(" Final volume: " + finalVolume);
+                System.out.println(" Final value: " + finalValue);
+
+                /*for (int x = 0; x < numlineups; x++) {
                     for (int i = 0; i < 6; i++) {
 
                     }
-                }
+                }*/
             }
         });
         SetButton.addActionListener(new ActionListener() {
@@ -105,6 +132,44 @@ public class PGA_Optimizer {
         try (Scanner rowScanner = new Scanner(line)) { rowScanner.useDelimiter(",");
             while (rowScanner.hasNext()) { values.add(rowScanner.next()); }
         } return values;
+    }
+
+    // Knapsack 0/1 without repetition
+    // Row: problem having only the first i items
+    // Col: problem having a knapsack of size j
+    // Third dimension: problem having a knapsack of volume h
+    private double[][][] dynNoRep;
+
+    private void noRep(PGA_Players[] items) {
+        dynNoRep = new double[items.length + 1][(int) (salary + 1)][(int) (maxprod + 1)];
+        for(int j = 0; j <= salary; j++) {
+            dynNoRep[0][j][0] = 0;
+        }
+        for(int i = 0; i <= maxprod; i++) {
+            dynNoRep[0][0][i] = 0;
+        }
+        for(int i = 0; i <= items.length; i++) {
+            dynNoRep[i][0][0] = 0;
+        }
+        for(int i = 1; i <= items.length; i++) {
+            for (int j = 0; j <= salary; j++) {
+                for (int h = 0; h <= maxprod; h++) {
+                    if (items[i - 1].Salary > j)
+                        // If the item i is too big, I  can't put it and the solution is the same of the problem with i - 1 items
+                        dynNoRep[i][j][h] = dynNoRep[i - 1][j][h];
+                    else {
+                        if (items[i - 1].Ownership > h)
+                            // If the item i is too voluminous, I can't put it and the solution is the same of the problem with i - 1 items
+                            dynNoRep[i][j][h] = dynNoRep[i - 1][j][h];
+                        else {
+                            // The item i could be useless and the solution is the same of the problem with i - 1 items, or it could be
+                            // useful and the solution is "(solution of knapsack of size j - item[i].size and volume h - item[i].volume) + item[i].value"
+                            dynNoRep[i][j][h] = Math.max(dynNoRep[i - 1][j][h], dynNoRep[i - 1][(int) (j - items[i - 1].Salary)][(int) (h - items[i - 1].Ownership)] + items[i - 1].Projection);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
