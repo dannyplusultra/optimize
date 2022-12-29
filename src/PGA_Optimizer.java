2c0c1708ebf74f4a12fe9200a98093915bd7e98c
nicobarossi <ngbarossi@gmail.com>
2022-12-29 14:40:46 -0800

    PGA_Optimizer

--- src/PGA_Optimizer.java
+++ src/PGA_Optimizer.java
@@ -2,14 +2,39 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class PGA_Optimizer {
     private JTabbedPane tabbedPane1;
     private JPanel PGA_Optimizer_Panel;
     private JPanel Start;
     private JButton Exit_Button;
-    private JButton fielButton;
+    private JButton fileButton;
+    private JButton RunButton;
+    private JPanel Rules;
+    private JTextField SalaryField;
+    private JButton SetButton;
+    private JTextField ProdOwnField;
+    private JTextField MaxSumOwn;
+    private JLabel UniquesPerLineup;
+    private JTextField MinUniques;
+    private JLabel NumLineups;
+    private JTextField NumLineupsField;
     private File file;
+
+    private double numlineups;
+
+    private double uniques;
+
+    private double maxsum;
+
+    private double maxprod;
+
+    private double salary;
+
+    private ArrayList <PGA_Players> Players;
 
     public PGA_Optimizer() {
     JFrame frame = new JFrame("PGA Optimizer");
@@ -27,17 +52,48 @@ public class PGA_Optimizer {
             }
         }
     });
-        fielButton.addActionListener(new ActionListener() {
+        fileButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(e.getSource()==fielButton) {
+                if(e.getSource()== fileButton) {
                    JFileChooser fileChooser = new JFileChooser();
-                   int respose = fileChooser.showOpenDialog(null);
+                   int response = fileChooser.showOpenDialog(null);
 
-                   if(respose == JFileChooser.APPROVE_OPTION){
+                   if(response == JFileChooser.APPROVE_OPTION){
                        file = new File(String.valueOf(fileChooser.getSelectedFile().getAbsoluteFile()));
+
+
+                       List<List<String>> records = new ArrayList<>();
+                       try (Scanner scanner = new Scanner(file);) {
+                           while (scanner.hasNextLine()) { records.add(getRecordFromLine(scanner.nextLine())); }
+                       }
+                       catch(Exception x){}
+                       for (List<String> player : records){
+                           Players.add(new PGA_Players(player.get(1), Double.parseDouble(player.get(5)), Double.parseDouble(player.get(9).replaceAll("%","")), Double.parseDouble(player.get(10))));
+                       }
+                       int o = 0;
                    }
                 }
+            }
+        });
+        RunButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (int x = 0; x < numlineups; x++) {
+                    for (int i = 0; i < 6; i++) {
+
+                    }
+                }
+            }
+        });
+        SetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                uniques = Double.parseDouble(MinUniques.getText());
+                maxprod = Double.parseDouble(ProdOwnField.getText());
+                maxsum = Double.parseDouble(MaxSumOwn.getText());
+                salary = Double.parseDouble(SalaryField.getText());
+                numlineups = Double.parseDouble(NumLineupsField.getText());
             }
         });
     }
@@ -45,4 +101,10 @@ public class PGA_Optimizer {
     public JPanel getPGA_Optimizer_Panel() {
         return PGA_Optimizer_Panel;
     }
+    private List<String> getRecordFromLine(String line) { List<String> values = new ArrayList<String>();
+        try (Scanner rowScanner = new Scanner(line)) { rowScanner.useDelimiter(",");
+            while (rowScanner.hasNext()) { values.add(rowScanner.next()); }
+        } return values;
+    }
 }
+
---
+++ src/PGA_Players.java
@@ -0,0 +1,13 @@
+public class PGA_Players {
+    public String NameID;
+    public double Salary;
+    public double Ownership;
+    public double Projection;
+
+    public PGA_Players(String nameID, double salary, double ownership, double projection){
+        NameID = nameID;
+        Salary = salary;
+        Ownership = ownership;
+        Projection = projection;
+    }
+}
