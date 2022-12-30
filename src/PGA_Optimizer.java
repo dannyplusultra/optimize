a244189a255bdc342e49a8eb09dff19e69115d9b
nicobarossi <ngbarossi@gmail.com>
2022-12-29 16:33:35 -0800

    PGA_Optimizer

--- src/PGA_Optimizer.java
+++ src/PGA_Optimizer.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -37,6 +38,7 @@ public class PGA_Optimizer {
     private ArrayList <PGA_Players> Players;
 
     public PGA_Optimizer() {
+        Players = new ArrayList<PGA_Players>();
     JFrame frame = new JFrame("PGA Optimizer");
     frame.setContentPane(PGA_Optimizer_Panel);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -55,6 +57,7 @@ public class PGA_Optimizer {
         fileButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                Players = new ArrayList<>();
                 if(e.getSource()== fileButton) {
                    JFileChooser fileChooser = new JFileChooser();
                    int response = fileChooser.showOpenDialog(null);
@@ -68,8 +71,9 @@ public class PGA_Optimizer {
                            while (scanner.hasNextLine()) { records.add(getRecordFromLine(scanner.nextLine())); }
                        }
                        catch(Exception x){}
+                       records.remove(0);
                        for (List<String> player : records){
-                           Players.add(new PGA_Players(player.get(1), Double.parseDouble(player.get(5)), Double.parseDouble(player.get(9).replaceAll("%","")), Double.parseDouble(player.get(10))));
+                           Players.add(new PGA_Players(player.get(1), Double.parseDouble(player.get(5)), (Double.parseDouble(player.get(9).replaceAll("%","")))/100, Double.parseDouble(player.get(10))));
                        }
                        int o = 0;
                    }
@@ -79,32 +83,32 @@ public class PGA_Optimizer {
         RunButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                PGA_Players[] items = (PGA_Players[]) Players.toArray();
-                System.out.print("We have the following " + items.length + " items (value, size, volume): ");
-                for(int i = 0; i < items.length; i++)
-                    System.out.print("(" + items[i].Projection + ", " + items[i].Salary + ", " + items[i].Ownership + ") ");
+                //PGA_Players[] items = (PGA_Players[]) Players.toArray();
+                System.out.print("We have the following " + Players.size() + " items (value, size, volume): ");
+                for(int i = 0; i < Players.size(); i++)
+                    System.out.print("(" + Players.get(i).Projection + ", " + Players.get(i).Salary + ", " + Players.get(i).Ownership + ") ");
                 System.out.println();
                 System.out.println("And a knapsack of size " + salary + " and volume " + maxprod);
 
-                noRep(items);
+                noRep(Players);
                 System.out.println();
                 // Print the solution
-                double j = salary, h = maxprod, finalSize = 0, finalValue = 0, finalVolume = 0;
+                double j = salary, h = maxprod, finalSalary = 0, finalProjection = 0, finalOwnership = 1;
                 System.out.print("Items picked (value, size, volume) for 0/1 problems without repetitions: ");
-                for(int i = items.length; i > 0; i--) {
+                for(int i = Players.size(); i > 0; i--) {
                     if(dynNoRep[i][(int)j][(int)h] != dynNoRep[i - 1][(int)j][(int)h]) {
-                        System.out.print("(" + items[i - 1].Projection + ", " + items[i - 1].Salary + ", " + items[i - 1].Ownership + ") ");
-                        finalSize += items[i - 1].Salary;
-                        finalValue += items[i - 1].Projection;
-                        finalVolume += items[i - 1].Ownership;
-                        j -= items[i - 1].Salary;
-                        h -= items[i - 1].Ownership;
+                        System.out.print("("+ Players.get(i-1).NameID + ", " + Players.get(i-1).Projection + ", " + Players.get(i-1).Salary + ", " + Players.get(i-1).Ownership + ") ");
+                        finalSalary += Players.get(i-1).Salary;
+                        finalProjection += Players.get(i-1).Projection;
+                        finalOwnership *= Players.get(i-1).Ownership;
+                        j -= Players.get(i-1).Salary;
+                        h -= Players.get(i-1).Ownership;
                     }
                 }
                 System.out.println();
-                System.out.println(" Final size: " + finalSize);
-                System.out.println(" Final volume: " + finalVolume);
-                System.out.println(" Final value: " + finalValue);
+                System.out.println(" Final size: " + finalSalary);
+                System.out.println(" Final Prod Ownership: " + finalOwnership);
+                System.out.println(" Final value: " + finalProjection);
 
                 /*for (int x = 0; x < numlineups; x++) {
                     for (int i = 0; i < 6; i++) {
@@ -140,31 +144,32 @@ public class PGA_Optimizer {
     // Third dimension: problem having a knapsack of volume h
     private double[][][] dynNoRep;
 
-    private void noRep(PGA_Players[] items) {
-        dynNoRep = new double[items.length + 1][(int) (salary + 1)][(int) (maxprod + 1)];
+    private void noRep(ArrayList<PGA_Players> items) {
+        maxprod = maxprod*10;
+        dynNoRep = new double[items.size() + 1][(int) (salary + 1)][(int) (maxprod + 1)];
         for(int j = 0; j <= salary; j++) {
             dynNoRep[0][j][0] = 0;
         }
         for(int i = 0; i <= maxprod; i++) {
             dynNoRep[0][0][i] = 0;
         }
-        for(int i = 0; i <= items.length; i++) {
+        for(int i = 0; i <= items.size(); i++) {
             dynNoRep[i][0][0] = 0;
         }
-        for(int i = 1; i <= items.length; i++) {
+        for(int i = 1; i <= items.size(); i++) {
             for (int j = 0; j <= salary; j++) {
                 for (int h = 0; h <= maxprod; h++) {
-                    if (items[i - 1].Salary > j)
+                    if (items.get(i-1).Salary > j)
                         // If the item i is too big, I  can't put it and the solution is the same of the problem with i - 1 items
                         dynNoRep[i][j][h] = dynNoRep[i - 1][j][h];
                     else {
-                        if (items[i - 1].Ownership > h)
+                        if (items.get(i-1).Ownership > h)
                             // If the item i is too voluminous, I can't put it and the solution is the same of the problem with i - 1 items
                             dynNoRep[i][j][h] = dynNoRep[i - 1][j][h];
                         else {
                             // The item i could be useless and the solution is the same of the problem with i - 1 items, or it could be
                             // useful and the solution is "(solution of knapsack of size j - item[i].size and volume h - item[i].volume) + item[i].value"
-                            dynNoRep[i][j][h] = Math.max(dynNoRep[i - 1][j][h], dynNoRep[i - 1][(int) (j - items[i - 1].Salary)][(int) (h - items[i - 1].Ownership)] + items[i - 1].Projection);
+                            dynNoRep[i][j][h] = Math.max(dynNoRep[i - 1][j][h], dynNoRep[i - 1][(int) (j - items.get(i-1).Salary)][(int) (h * items.get(i-1).Ownership)] + items.get(i-1).Projection);
                         }
                     }
                 }
